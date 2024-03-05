61b8d477541c028ac12370190af8a0e5a04c2986
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-05 13:39:26 +0100

    feat: create model and migrate file for order - [Database] TABLE : orders #27

---
+++ database/factories/OrderFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Address;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Order>
+ */
+class OrderFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'order_number' => $this->faker->sentence(1, true),
+            'order_date' => $this->faker->dateTime('now'),
+            'status' => $this->faker->sentence(1, true),
+            'user_id' => $this->faker->randomElement(User::pluck('id')),
+            'billing_address_id' => $this->faker->randomElement(Address::pluck('id')),
+            'delivery_address_id' => $this->faker->randomElement(Address::pluck('id')),
+        ];
+    }
+}
