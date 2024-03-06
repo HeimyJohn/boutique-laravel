2a8372bfd8cd0bd4fe4a094990f292926eb29e18
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-06 15:50:18 +0100

    feat: create controller and view for product file - [PAGES] Products #53

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +15,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/product/{id}', [ProductController::class, 'show'])->name('product');
