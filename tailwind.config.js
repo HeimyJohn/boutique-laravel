7c1c9f973d4b5c9757fbaf5b5ce265ce8275a387
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-07 14:35:23 +0100

    feat: change code in tailwind.config, Category and Product files - [PAGES] - change code #67

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,50 +1,37 @@
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: [
-//     "./resources/**/*.blade.php",
-//     "./resources/**/*.js",
-//     "./resources/**/*.vue",
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [require("daisyui")],
-// }
+
 export default {
-    content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
-    ],
-    theme: {
-        extend: {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+  ],
+  theme: {
+    extend: {
+      height: {
+        '168': '42rem',
+      },
 
-            colors: {
-                "yellow-figma": "#ffd400", // yellow
-                "purple-figma": "#5540bf", // purple
-                "blue-figma": "#209fdf", // blue
-                "white-figma": "#f7f7f7", // white
-                "black-figma": "#080808", // white
-                "gray-figma": "#3e3e3e", // gray
-                "blue-variant-figma": "#005e8d", // blue-variant
-                "pink-figma": "#e96379", // pink
-            },
-            dropShadow: {
-                'white': '0 0px 50px rgb(180, 180, 180, 0.5)',
-            },
+      colors: {
+        "yellow-figma": "#ffd400", // yellow
+        "purple-figma": "#5540bf", // purple
+        "blue-figma": "#209fdf", // blue
+        "white-figma": "#f7f7f7", // white
+        "black-figma": "#080808", // white
+        "gray-figma": "#3e3e3e", // gray
+        "blue-variant-figma": "#005e8d", // blue-variant
+        "pink-figma": "#e96379", // pink
+      },
 
-            fontFamily: {
-                ['serif']: ['Roboto'],
-                ['sans']: ['Roboto'],
-                ['mono']: ['Roboto'],
-            },
-
-        },
+      dropShadow: {
+        'white': '0 0px 50px rgb(180, 180, 180, 0.5)',
     },
 
-    plugins: [
-        require("daisyui"),
-        require('@tailwindcss/forms'),
-    ],
-
+    fontFamily: {
+        ['serif']: ['Roboto'],
+        ['sans']: ['Roboto'],
+        ['mono']: ['Roboto'],
+    },
+    },
+  },
+  plugins: [require("daisyui"), require('@tailwindcss/forms'),],
 }
