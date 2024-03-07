0713557a3db03031db2352afa2af9e2d671875c1
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-07 15:45:09 +0100

    feat: change code

--- tailwind.config.js
+++ tailwind.config.js
@@ -33,5 +33,8 @@ export default {
     },
     },
   },
-  plugins: [require("daisyui"), require('@tailwindcss/forms'),],
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/forms'),
+  ],
 }
