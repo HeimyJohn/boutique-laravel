ee4b894ff4c7218f2f69fa409654cc2f5aa0c724
florent sartout <florent.sartout@le-campus-numerique.fr>
2024-03-11 16:05:14 +0100

    refactor: remake session controller and test for add product and verify if product exists. If exists, add quantity of 1

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,43 +2,56 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
 
 class SessionController extends Controller
 {
-    // public function getCardSession(Request $request)
+    // public function getCartSession()
     // {
-    //     if ($request->session()->has('id_product')) {
-    //         echo $request->session()->get('id_product');
+    //     if (Session::has('product')) {
+    //         for ($i = 0; $i < count(Session::get('product')); $i++) {
+    //             $products[$i] = Product::select('*')->where('id',  '=',  Session::get('product.' . $i . '.id'))->first();
+    //         }
     //     }
 
-    //     if ($request->session()->has('quantity')) {
-    //         echo $request->session()->get('quantity');
-    //     }
+    //     return $products;
     // }
 
-    public function setCardSession(Request $request)
+    public function setCartSession(Request $request)
     {
-        // Session::flush('id_product', 'quantity', 'id', 'product');
+        // Session::flush('id_product', 'quantity', 'id', 'product, products');
 
         $id = $request->input('id');
+        $idExists = null;
 
-        if(Session::has('product')) {
-            for($i = 0; $i < count(Session::get('product')); $i++) {
-                if($id != Session::get('product.' . $i . '.id')) {
-                    Session::push('product', ['id' => $id, 'quantity' => 1]);
+        if (Session::has('products')) {
+            foreach (Session::get('products') as $session) {
+                if ($id === $session['id']) {
+                    $idExists = true;
                     break;
                 }
             }
+
+            foreach (Session::get('products') as $key => $value) {
+                if ($idExists) {
+                    if ($id === $session['id']) {
+                        session(["products.$key.quantity" => $session['quantity'] + 1]);
+                        break;
+                    }
+                }
+            }
+
+            Session::push('products', ['id' => $id, 'quantity' => 1]);
         } else {
-            Session::push('product', ['id' => $id, 'quantity' => 1]);
+            Session::push('products', ['id' => $id, 'quantity' => 1]);
         }
 
-        return redirect()->route('card');
+        return redirect()->route('cart');
     }
 
-    // public function deleteCardSession(Request $request)
+    // public function deleteCartSession(Request $request)
     // {
     //     $request->session()->forget('id_product');
     //     $request->session()->forget('quantity');
